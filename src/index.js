f26ad861f4882bcd5439b56e494113198d5268bd
Aliaksei Shytkin <e79eas@gmail.com>
2015-07-16 17:32:18 +0300

    Make better output

--- src/index.js
+++ src/index.js
@@ -2,10 +2,33 @@ import cu from 'targetprocess-mashup-helper/lib/customUnits';
 
 const id = 'customunit_last_comment';
 
+const output = (text) => {
+
+    const tmp = document.createElement("DIV");
+
+    tmp.innerHTML = text;
+
+    let plain = tmp.textContent || tmp.innerText || "";
+
+    if (plain.length > 150) {
+
+        plain = plain.substring(0, 150) + "...";
+
+    }
+
+    return plain.replace(/\u00a0/g, " ");
+};
+
 cu.add({
     id: id,
     name: 'Last Comment',
-    template: '<div class="tau-board-unit__value"><%= this.data.comment.description %></div>',
+    outerClassName: 'tau-board-unit_type_custom-rich-tex',
+    alignment: 'base',
+    sections: 1,
+    template: {
+        markup: '<div class="tau-board-unit__value" style="white-space:normal"><%= fn.output(this.data.comment.description) %>',
+        customFunctions: {output}
+    },
     hideIf: function(data) {
         return !data.comment;
     },
@@ -14,7 +37,7 @@ cu.add({
     },
     sampleData: {
         comment: {
-            description: 'hello here'
+            description: 'Suspendisse consequat nisi et diam euismod, vel pulvinar massa laoreet. Nunc finibus et elit nec ultrices. Suspendisse consectetur tincidunt tortor, venenatis varius dolor.'
         }
     }
 });
